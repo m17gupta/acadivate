c587e93cb6d21f4509d260aad006117eb50d4567
MANISH KUMAR GUPTA <manish.gupta.mech@gmail.com>
2026-04-21 15:20:30 +0530

    update

--- src/app/nomination-form/[id]/page.tsx
+++ src/app/nomination-form/[id]/page.tsx
@@ -2,7 +2,6 @@ import NominationForm from '@/src/components/forms/Nomination/NominationForm'
 import React from 'react'
 
 const page = async ({params}: {params: Promise<{id: string}>}) => {
-  const { id } = await params
 
   return (
   <>
